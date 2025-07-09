31a3935d9577a930e26661b5bf6264355a59e856
Quietbothered <mukesh2002king@gmail.com>
2025-07-09 10:31:58 +0530

    d14
    selenium handle links handle broken links

--- selenium_practice/handlelinks.py
+++ selenium_practice/handlelinks.py
@@ -7,9 +7,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
 import time
+
 serv_obj = Service(r"C:\Driver\chromedriver-win64\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=serv_obj)
 
 driver.get("https://demo.nopcommerce.com/")
 driver.maximize_window()#maximize browser window
+
+
+links = driver.find_elements(By.TAG_NAME, 'a')
+print('total number of links', len(links))
+for link in links :
+    print (link.text)
+    print(link.get_attribute('value'))
 
